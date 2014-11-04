e9345d467029862058e03f6e82b9503fd3641a17
Diego Vargas <diego@tradesy.com>
2014-11-04 10:58:48 -0800

    otherstuffs

--- init.el
+++ init.el
@@ -129,5 +129,26 @@
 ;;;(load-theme 'solarized-light t)
 (load-theme 'solarized-dark t)
 
+;;;scala mode stuffs
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
+(unless (package-installed-p 'scala-mode2)
+  (package-refresh-contents) (package-install 'scala-mode2))
 
-
+;;;ido mode
+(require 'ido)
+(ido-mode t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ido-enable-flex-matching t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
